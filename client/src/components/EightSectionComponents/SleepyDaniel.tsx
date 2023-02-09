2d34c4711a3db050fa87683b621366dff2dcad04
Ariel Behar <ariel.behar@hotmail.com>
2023-02-09 19:33:29 +0200

    creating the danielProject file and importing its data into the SleepyDaniel component

--- client/src/components/EightSectionComponents/SleepyDaniel.tsx
+++ client/src/components/EightSectionComponents/SleepyDaniel.tsx
@@ -1,12 +1,15 @@
+import danielProject from '../../data/danielProject.json'
+
+import styled from "@mui/material/styles/styled"
+
 import Box from "@mui/material/Box"
 import Container from "@mui/material/Container"
 import Grid from "@mui/material/Grid"
 import Link from "@mui/material/Link"
 import Typography from "@mui/material/Typography"
+import Stack from "@mui/material/Stack"
 
 import CloseIcon from '@mui/icons-material/Close';
-import styled from "@mui/material/styles/styled"
-import Stack from "@mui/material/Stack"
 
 const StyledCloseIcon = styled(CloseIcon)`
     position: absolute;
@@ -16,55 +19,52 @@ const StyledCloseIcon = styled(CloseIcon)`
     left: 20px;
     cursor: pointer;
 `
-
 interface Props {
     onClickShowDanielHandler: (bool: boolean) => void;
 }
-
-const technologies = ['HTML', 'CSS', 'XML']
 
 function SleepyDaniel({ onClickShowDanielHandler }: Props) {
     return (
         <Box sx={{ background: "rgb(138, 176, 218)" }}>
             <Container sx={{ position: 'relative' }}>
                 <StyledCloseIcon onClick={() => onClickShowDanielHandler(false)} />
-                    <Grid container >
-                        <Grid item md={7} p={7}>
-                            <Typography variant="h4" component='h4' color='text.primary' mb={7} textAlign='center'>Sleepy Daniel</Typography>
+                <Grid container >
+                    <Grid item md={7} p={7}>
+                        <Typography variant="h4" component='h4' color='text.primary' mb={7} textAlign='center'>{danielProject.title}</Typography>
 
-                            <Link href='http://sleepydaniel.arielbehar.com/' target='_blank'>
-                                <img style={{maxWidth: "100%", height: 'auto'}} src="https://arielbehar-portfolio.s3.eu-central-1.amazonaws.com/projects-slides/daniel.jpg" alt="Sleepy Daniel" />
-                            </Link>
+                        <Link href={danielProject.url} target='_blank'>
+                            <img style={{ maxWidth: "100%", height: 'auto' }} src={`https://arielbehar-portfolio.s3.eu-central-1.amazonaws.com/projects-slides/${danielProject.image}`} alt="Sleepy Daniel" />
+                        </Link>
 
-                        </Grid>
-                        <Grid item md={5}>
+                    </Grid>
+                    <Grid item md={5}>
                         <Stack>
 
-							<Typography mt={2} variant='h6' component='h6' color='text.primary'>Project Description:</Typography>
-							<Typography variant='body1' component='p' color='text.secondary' textAlign='justify'>Biography site of Daniel Zalmen Jackson. The site aims to accurately portray Daniel's early days with the help of visual and audio aids</Typography>
+                            <Typography mt={2} variant='h6' component='h6' color='text.primary'>Project Description:</Typography>
+                            <Typography variant='body1' component='p' color='text.secondary' textAlign='justify'>{danielProject.description}</Typography>
 
-							<Typography mt={2} variant='h6' component='h6' color='text.primary'>My Role in the Project:</Typography>
-							<Typography variant='body1' component='p' color='text.secondary' textAlign='justify'>This is the first website I ever built. It contains pure HTML, CSS and a tiny bit of XML. Making it responsive or cross-browser compatible were not even options in the scope of possibilities at this point, but I was very excited to make my first web animations. Overall, an exciting project for its time.</Typography>
+                            <Typography mt={2} variant='h6' component='h6' color='text.primary'>My Role in the Project:</Typography>
+                            <Typography variant='body1' component='p' color='text.secondary' textAlign='justify'>{danielProject.myRole}</Typography>
 
-							<Box>
-								<Typography mt={2} variant='h6' component='h6' color='text.primary'>Technologies Used:</Typography>
+                            <Box>
+                                <Typography mt={2} variant='h6' component='h6' color='text.primary'>Technologies Used:</Typography>
 
-								{technologies.map(technology => <Typography variant='body1' component='p' color='text.secondary' display='inline-block'>•&nbsp;{technology}&nbsp;</Typography>)}
-							</Box>
+                                {danielProject.technologies.map(technology => <Typography variant='body1' component='p' color='text.secondary' display='inline-block'>•&nbsp;{technology}&nbsp;</Typography>)}
+                            </Box>
 
-							<Typography mt={1} variant='h6' component='h6' color='text.primary'>Project Launched in:</Typography>
-							<Typography variant='body1' component='p' color='text.secondary'>March 2017</Typography>
+                            <Typography mt={1} variant='h6' component='h6' color='text.primary'>Project Launched in:</Typography>
+                            <Typography variant='body1' component='p' color='text.secondary'>{danielProject.month}&nbsp;{danielProject.year}</Typography>
 
-							<Typography mt={2} variant='h6' component='h6' color='text.primary'>
-								Visit site:&nbsp;
-								<Link href='http://sleepydaniel.arielbehar.com/' target='_blank' color='text.secondary'>Sleepy Daniel</Link>
-							</Typography>
+                            <Typography mt={2} variant='h6' component='h6' color='text.primary'>
+                                Visit site:&nbsp;
+                                <Link href={danielProject.url} target='_blank' color='text.secondary'>{danielProject.title}</Link>
+                            </Typography>
 
                             <Typography variant='body1' component='p' color='text.muted' mt={0}>(best experienced in Google Chrome)</Typography>
-						</Stack>
+                        </Stack>
 
-                        </Grid>
                     </Grid>
+                </Grid>
             </Container>
         </Box>
     )
