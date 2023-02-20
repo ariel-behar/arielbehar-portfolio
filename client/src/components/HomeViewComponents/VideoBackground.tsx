ad67c92aa88fb8568378148e01bf41f885b8099b
Ariel Behar <ariel.behar@hotmail.com>
2023-02-20 22:56:37 +0200

    homeView video background now loops properly and has a poster

--- client/src/components/HomeViewComponents/VideoBackground.tsx
+++ client/src/components/HomeViewComponents/VideoBackground.tsx
@@ -2,6 +2,7 @@ import inkMP4 from '../../assets/video/ink.mp4'
 import inWEBM from '../../assets/video/ink.webm'
 import inkOGV from '../../assets/video/ink.ogv'
 import styled from '@mui/material/styles/styled'
+import { useRef } from 'react'
 
 const StyledVideo = styled('video')`
     position: relative;
@@ -12,15 +13,20 @@ const StyledVideo = styled('video')`
 `
 
 function VideoBackground() {
+    const videoRef = useRef<HTMLVideoElement>(null)
+
+    const onVideoEndedHandler = async (e: React.SyntheticEvent<HTMLVideoElement> ) => {
+        videoRef.current?.load()
+    }
+
     return (
         <>
-            <StyledVideo poster="img/downloaded/ink.png" loop muted autoPlay>
+            <StyledVideo ref={videoRef} poster="https://arielbehar-portfolio.s3.eu-central-1.amazonaws.com/backgrounds/ink-poster.jpg" muted autoPlay playsInline onEnded={onVideoEndedHandler}>
 				<source src={inkMP4} type="video/mp4" />
 				<source src={inWEBM} type="video/webm" />
 				<source src={inkOGV} type="video/ogv" />
+                Your browser does not support the video tag.
 			</StyledVideo>
-
-			<img src="img/downloaded/ink2.png" alt="Ink Poster" id="ink-poster" />
         </>
     )
 }
