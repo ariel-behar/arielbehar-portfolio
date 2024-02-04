6b213c36c70ddaabe265fded8fe4a88730f784e3
Ariel Behar <ariel.behar@hotmail.com>
2024-02-04 14:59:55 +0200

    Removing dist folder from repo

--- server/dist/routes/routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import formController from '../controllers/formController.js';
-const router = Router();
-router.use('/form', formController);
-export default router;
-//# sourceMappingURL=routes.js.map
