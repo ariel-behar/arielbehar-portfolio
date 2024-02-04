b4c31404d819720bd5a8b01d01019fcec39b3ee8
Ariel Behar <ariel.behar@hotmail.com>
2024-02-04 19:00:33 +0200

    Installing and implementing disable React Dev Tools

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Suspense, lazy } from "react";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 import theme from "./theme/theme";
 
@@ -7,6 +8,8 @@ import ThemeProvider from "@mui/material/styles/ThemeProvider";
 
 import Loader from "./views/Loader";
 const RootView = lazy(() => import("./views/RootView")) 
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools();
 
 function App() {
 	return (
