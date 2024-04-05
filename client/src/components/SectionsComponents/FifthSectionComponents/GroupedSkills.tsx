d0cebf4e01eb5b060126ac0fa1917286efc50ac7
Ariel Behar <ariel.behar@hotmail.com>
2024-04-05 09:21:17 +0300

    Creating a GroupedSkills display variant component

---
+++ client/src/components/SectionsComponents/FifthSectionComponents/GroupedSkills.tsx
@@ -0,0 +1,38 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import { Skill, Technology } from "../../../model/Skill"
+import Chip from './Chip';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+
+interface Props {
+    skills: Skill[],
+    hoveredProficiency: 1 | 2 | 3 | null
+}
+
+function GroupedSkills({ skills, hoveredProficiency }: Props) {
+    return (
+        <>
+            {
+                skills.map((skill: Skill) => {
+                    return (
+                        <Box mt={1} borderLeft={{ xs: 'none', md: '1px dashed #e0e0e070' }} pl={{ xs: 0, md: 1 }}>
+                            <Typography variant='body2' component="h6" color="text.secondary" key={uuidv4()}>{skill.title}</Typography>
+
+                            <Stack direction='row' flexWrap='wrap' justifyContent='left'>
+                                {
+                                    skill['technologies'].map((technology: Technology) => {
+                                        return technology.displayTechnology === true && <Chip key={uuidv4()} technology={technology} hoveredProficiency={hoveredProficiency} />
+                                    })
+                                }
+                            </Stack>
+                        </Box>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+export default GroupedSkills
