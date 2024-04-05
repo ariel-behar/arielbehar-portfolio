45186e22e527110b6606a2c693c4240785cd112b
Ariel Behar <ariel.behar@hotmail.com>
2024-04-05 12:25:31 +0300

    Adding a background behind the skills and changing the section's structure

--- client/src/views/MainViewSections/FifthAbilitiesSection.tsx
+++ client/src/views/MainViewSections/FifthAbilitiesSection.tsx
@@ -2,31 +2,28 @@ import { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
 import skills from '../../data/skills.json'
-import { Skill, Technology } from "../../model/Skill"
+import arrowRight from '../../assets/curved-arrow.png'
 
 import styled from "@mui/material/styles/styled"
 
 import Box from "@mui/material/Box"
-import Grid from "@mui/material/Grid"
 import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography"
 
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import Chip from '../../components/SectionsComponents/FifthSectionComponents/Chip'
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import ProficiencyTitle from '../../components/SectionsComponents/FifthSectionComponents/ProficiencyTitle';
-import AllSkills from '../../components/SectionsComponents/FifthSectionComponents/AllSkills';
 import GroupedSkills from '../../components/SectionsComponents/FifthSectionComponents/GroupedSkills';
 
-const StyledKeyboardArrowDownIcon = styled(KeyboardArrowDownIcon)`
+const StyledKeyboardArrowRightIcon = styled(KeyboardArrowRightIcon)`
 	color: ${(({ theme }) => theme.palette.text.muted.light)};
 
 	position: relative;
-	animation: pointDown 1s infinite;
+	animation: pointDown 1.5s infinite ease-in-out;
 
 	@keyframes pointDown {
-		0% {top: 0}
-		50% {top: 3px}
-		100% {top: 0}
+		0% {left: 0}
+		50% {left: 3px}
+		100% {left: 0}
 	}
 `
 
@@ -67,45 +64,44 @@ function FifthAbilitiesSection({ loadSectionHandler }: Props) {
 
 	return (
 		<Box component='section' id="fifth-section">
-			<Typography mt={3} variant="h2" component='h3' color="text.secondary" textAlign='center' display={{ xs: 'block', md: 'none' }}>My Abilities</Typography>
-			<Grid container spacing={{ xs: 1, lg: 5 }} py={{ xs: 2, lg: 5 }}>
-				<Grid item xs={12} sm={12} md={12} textAlign="center">
-					<Stack pt={{ xs: 0, lg: 3 }} direction="row" alignItems='center'>
 
-						<Typography variant="h2" component='h3' color="text.secondary" textAlign='center' display={{ xs: 'none', md: 'block' }}>My Abilities</Typography>
+			<Stack pt={{ xs: 2, md: 3 }} pb={{ xs: 0, md: 3 }} direction={{ xs: 'column', md: 'row' }} justifyContent={{ xs: 'center', md: 'space-between' }} alignItems='center'>
 
+				<Typography variant="h2" component='h3' color="text.secondary" textAlign='center'>My Abilities</Typography>
 
-
-
-
-						<Stack direction={{ xs: 'row', md: 'row' }} justifyContent='space-around' mt={{ xs: 0, md: 12, lg: 3 }}>
-
-							<Typography variant="body1" component='p' color="text.muted.light" display={{ xs: 'none', lg: 'block' }}>HOVER</Typography>
-							<StyledKeyboardArrowDownIcon sx={{ display: { xs: 'none', lg: 'block' } }} />
-
-							{
-								skillTitles.map(skillTitle => {
-									return <ProficiencyTitle
-										key={uuidv4()}
-										hoveredProficiency={hoveredProficiency}
-										skillProficiencyNum={skillTitle.proficiencyNum}
-										onMouseEnterProficiencyTitleHandler={onMouseEnterProficiencyTitleHandler}
-										onMouseLeaveProficiencyTitleHandler={onMouseLeaveProficiencyTitleHandler}
-									>
-										{skillTitle.title}
-									</ProficiencyTitle>
-								})
-							}
-						</Stack>
+				<Stack direction='row' justifyContent='space-around' >
+					<Stack direction='column' justifyContent='center' alignItems='center' pr={2}>
+						<Typography variant="body2" component='p' color="text.muted.light" display={{ xs: 'none', lg: 'block' }}>HOVER</Typography>
+						<StyledKeyboardArrowRightIcon sx={{ display: { xs: 'none', lg: 'block' } }} />
 					</Stack>
-				</Grid>
 
-				<Grid item xs={12} sm={12} md={12} >
-					<GroupedSkills skills={skills} hoveredProficiency={hoveredProficiency} />
+					{
+						skillTitles.map(skillTitle => {
+							return <ProficiencyTitle
+								key={uuidv4()}
+								hoveredProficiency={hoveredProficiency}
+								skillProficiencyNum={skillTitle.proficiencyNum}
+								onMouseEnterProficiencyTitleHandler={onMouseEnterProficiencyTitleHandler}
+								onMouseLeaveProficiencyTitleHandler={onMouseLeaveProficiencyTitleHandler}
+							>
+								{skillTitle.title}
+							</ProficiencyTitle>
+						})
+					}
+				</Stack>
+			</Stack>
+			<Box
+				sx={{
+					backgroundImage: "url('https://arielbehar-portfolio.s3.eu-central-1.amazonaws.com/backgrounds/curved-arrow.png')",
+					backgroundRepeat: 'no-repeat',
+					backgroundPosition: 'right bottom',
+				}}
 
-					<Typography mt={1} variant="body1" component='p' color="text.muted.main" textAlign='center' >*In constant learning of new skills and improvement on already existing ones</Typography>
-				</Grid>
-			</Grid>
+			>
+				<GroupedSkills skills={skills} hoveredProficiency={hoveredProficiency} />
+
+				<Typography mt={1} variant="body1" component='p' color="text.muted.main" textAlign='center' >*In constant learning of new skills and improvement on already existing ones</Typography>
+			</Box>
 		</Box>
 	)
 }
