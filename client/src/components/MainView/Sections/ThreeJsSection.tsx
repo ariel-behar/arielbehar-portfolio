f6117fcf3e8a9606107d0a946d330f2436b959df
Ariel Behar <ariel.behar@hotmail.com>
2024-05-28 21:55:20 +0300

    Creating the initial structure of the ThreeJsSection component

--- client/src/components/MainView/Sections/ThreeJsSection.tsx
+++ client/src/components/MainView/Sections/ThreeJsSection.tsx
@@ -1,20 +1,26 @@
 import { useEffect } from "react"
 
+import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
+
 interface Props {
-  loadSectionHandler: (sectionNum: number) => void
+	loadSectionHandler: (sectionNum: number) => void
 }
 
 function ThreeJsSection({
-  loadSectionHandler
+	loadSectionHandler
 }: Props) {
 
-  useEffect(() => {
-    loadSectionHandler(5)
-  }, [loadSectionHandler])
+	useEffect(() => {
+		loadSectionHandler(5)
+	}, [loadSectionHandler])
 
-  return (
-    <div>ThreeJsProjects</div>
-  )
+	return (
+		<Box component="section">
+			<Typography variant="h4" component='h4' mt={0} textAlign='center' color='text.secondary'>Three JS Projects</Typography>
+
+		</Box>
+	)
 }
 
 export default ThreeJsSection
