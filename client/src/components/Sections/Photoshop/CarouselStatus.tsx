245ed07d3f18eba5c0fb15900d759b058a8a1df3
Ariel Behar <ariel.behar@hotmail.com>
2024-05-28 10:36:08 +0300

    Creating the CarouselStatus component

---
+++ client/src/components/Sections/Photoshop/CarouselStatus.tsx
@@ -0,0 +1,32 @@
+import Box from "@mui/material/Box"
+
+import Typography from "@mui/material/Typography"
+import Project from "../../../model/Project"
+
+interface Props {
+    currentSlide: number,
+    totalSlides: number
+}
+
+function CarouselStatus({
+    currentSlide,
+    totalSlides
+}: Props) {
+    return (
+        <Box sx={{ position: 'relative' }}>
+            <Typography variant="body1" component={"p"} sx={{
+                position: "absolute",
+                top: "5px",
+                right: "10px",
+                padding: "5px",
+                fontSize: "16px",
+                textShadow: "1px 1px 1px rgba(0, 0, 0, 0.9)",
+                color: "#fff"
+            }} >
+                {currentSlide} of {totalSlides}
+            </Typography>
+        </Box>
+    )
+}
+
+export default CarouselStatus
