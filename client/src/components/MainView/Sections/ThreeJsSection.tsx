fb1cfed1988743aef3ed4f676de3e241d544868f
Ariel Behar <ariel.behar@hotmail.com>
2024-05-31 13:45:10 +0300

    Commenting out the first grid item in the ThreeJsSection

--- client/src/components/MainView/Sections/ThreeJsSection.tsx
+++ client/src/components/MainView/Sections/ThreeJsSection.tsx
@@ -25,24 +25,26 @@ function ThreeJsSection({
 	}, [loadSectionHandler])
 
 	return (
-		<Box component="section" py={3} position='relative'>
-			<Typography variant="h4" component='h4' textAlign='center' color='text.secondary'>Three JS</Typography>
+		<Box component="section" py={3} >
+			<Box position='relative'>
+				<Typography variant="h4" component='h4' textAlign='center' color='text.secondary'>Three JS</Typography>
 
-			<CarouselStatus totalSlides={projects.length} currentSlide={projects.length} />
+				<CarouselStatus totalSlides={projects.length} currentSlide={projects.length} />
 
-			<Grid container columnSpacing={1} rowSpacing={2} my={3}>
-				{/* <Grid item lg={6}>
+				<Grid container columnSpacing={1} rowSpacing={2} my={3}>
+					{/* <Grid item lg={6}>
 					</Grid> */}
-				{
-					(projects as ThreeJsProject[]).map((project, index) => {
-						return (
-							<Grid item xs={6} sm={6} md={4} lg={3} key={uuiqd()}>
-								<ThreeJsCard project={project} />
-							</Grid>
-						)
-					}
-					)}
-			</Grid>
+					{
+						(projects as ThreeJsProject[]).map((project, index) => {
+							return (
+								<Grid item xs={6} sm={6} md={4} lg={3} key={uuiqd()}>
+									<ThreeJsCard project={project} />
+								</Grid>
+							)
+						}
+						)}
+				</Grid>
+			</Box>
 		</Box>
 	)
 }
