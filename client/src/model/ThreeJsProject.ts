e1babcfb4c6d846e8a1e12657e08126d72f3ff40
Ariel Behar <ariel.behar@hotmail.com>
2024-05-31 09:45:36 +0300

    Creating the ThreeJsProject model

---
+++ client/src/model/ThreeJsProject.ts
@@ -0,0 +1,22 @@
+import { Identifiable, IdType } from "../types/common-types"
+
+interface ThreeJsProject extends Identifiable<IdType> {
+    _id: IdType
+    order: number,
+    title: string,
+    image: string,
+    description: string,
+    myRole?: string,
+    technologies: string[],
+    month: string,
+    year: number,
+    url: string,
+    notes: [] | [
+        {
+            note: string,
+            hasLink?: string
+        }
+    ]
+}
+
+export default ThreeJsProject
