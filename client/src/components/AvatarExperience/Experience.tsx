783693b8dda4264a697e2bd66f2b1f727945c0da
Ariel Behar <ariel.behar@hotmail.com>
2024-06-05 20:50:41 +0300

    Adding presentation controls to the Experience

--- client/src/components/AvatarExperience/Experience.tsx
+++ client/src/components/AvatarExperience/Experience.tsx
@@ -1,25 +1,34 @@
-import { OrbitControls } from '@react-three/drei'
-import {useControls} from 'leva'
+// import { OrbitControls } from '@react-three/drei'
+import { PresentationControls } from '@react-three/drei'
+import { useControls } from 'leva'
 import * as THREE from 'three'
 
 import Avatar, { ActionName } from './Avatar/Avatar'
 
 function Experience() {
-	const {animation }= useControls({
+	const { animation } = useControls({
 		animation: {
-			value: 'Crouch',
+			value: 'Idle',
 			options: ['Crouch', 'Idle', 'Wave']
 		}
 	})
 
 	return (
 		<>
-			<OrbitControls />
-			<group position-y={-1}>
-				<Avatar animation={animation as ActionName}/>
-			</group>
+			<PresentationControls
+				global
+				rotation={[0.13, 0.1, 0]}
+				polar={[0, 0]}
+				azimuth={[- 1.5, 0.25]}
+				// config={{ mass: 2, tension: 400 }}
+				// snap={{ mass: 4, tension: 400 }}
+			>
+				<group position-y={-1}>
+					<Avatar animation={animation as ActionName} />
+				</group>
 
-			<ambientLight intensity={4} />
+				<ambientLight intensity={4} />
+			</PresentationControls>
 		</>
 	)
 }
