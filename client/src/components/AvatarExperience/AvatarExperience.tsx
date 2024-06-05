a5d14d9fd43b982eb8b3ef8d9354991975670f4d
Ariel Behar <ariel.behar@hotmail.com>
2024-06-05 19:03:20 +0300

    Adding StrictMode to the AvatarExperience

--- client/src/components/AvatarExperience/AvatarExperience.tsx
+++ client/src/components/AvatarExperience/AvatarExperience.tsx
@@ -1,20 +1,24 @@
 import { Canvas } from "@react-three/fiber"
 import Experience from "./Experience"
+import { StrictMode } from "react"
 
 
 function AvatarExperience() {
     return (
-        <Canvas
-            camera={{
-                fov: 30,
-                near: 0.1,
-                far: 1000,
-                position: [0, 1, 4]
-            }}
+        <StrictMode>
+            <Canvas
+                camera={{
+                    fov: 30,
+                    near: 0.1,
+                    far: 1000,
+                    position: [0, 1, 4]
+                }}
 
-        >
-            <Experience />
-        </Canvas>
+            >
+
+                <Experience />
+            </Canvas>
+        </StrictMode>
     )
 }
 
