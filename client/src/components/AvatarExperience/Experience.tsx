ab0d28573c512ed8dee578a406b465b0bcf220b0
Ariel Behar <ariel.behar@hotmail.com>
2024-06-05 19:04:21 +0300

    Using leva to choose an animation in the Experience component

--- client/src/components/AvatarExperience/Experience.tsx
+++ client/src/components/AvatarExperience/Experience.tsx
@@ -1,14 +1,22 @@
 import { OrbitControls } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
+import {useControls} from 'leva'
+import * as THREE from 'three'
 
-import Avatar from './Avatar/Avatar'
+import Avatar, { ActionName } from './Avatar/Avatar'
 
 function Experience() {
+	const {animation }= useControls({
+		animation: {
+			value: 'crouchToStand',
+			options: ['crouchToStand', 'breathingIdle', 'wavingGesture']
+		}
+	})
+
 	return (
 		<>
 			<OrbitControls />
 			<group position-y={-1}>
-				<Avatar />
+				<Avatar animation={animation as ActionName}/>
 			</group>
 
 			<ambientLight intensity={4} />
