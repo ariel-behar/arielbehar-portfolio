b630ccf027b7064adc8cbf5cfc03527faf59ac6a
Ariel Behar <ariel.behar@hotmail.com>
2024-06-05 06:32:23 +0300

    Moving the animation buttons into the ThreeJsProjectsExperience component

--- client/src/components/AvatarExperience/Experience.tsx
+++ client/src/components/AvatarExperience/Experience.tsx
@@ -1,52 +1,42 @@
-import { Environment, PresentationControls } from '@react-three/drei'
-import { useControls } from 'leva'
+
+import { Environment } from '@react-three/drei'
+import { Suspense } from 'react'
 import * as THREE from 'three'
 
-import Avatar from './Avatar/Avatar'
+import Avatar, { ActionName } from './Avatar/Avatar'
 import Harley from './Harley/Harley'
 
 interface Props {
-	isInView: boolean
+	isInView: boolean,
+	animationName: ActionName | '',
 }
 
 function Experience({
-	isInView
+	isInView,
+	animationName
 }: Props) {
-	const { envMapIntensity, envMapHeight, envMapRadius, envMapScale } = useControls('environment map', {
-		envMapIntensity: { value: 1.3, min: 0, max: 12 },
-		envMapHeight: { value: 9, min: 0, max: 100 },
-		envMapRadius: { value: 48, min: 10, max: 1000 },
-		envMapScale: { value: 6, min: 5, max: 1000 }
-	})
+	
 
 	return (
-		<PresentationControls
-			global
-			rotation={[0.13, 0.1, 0]}
-			polar={[0, 0]}
-			azimuth={[- 0.35, 0.35]}
-			config={{ mass: 5, tension: 400 }}
-		// snap={{ mass: 4, tension: 400 }}
-		>
-			<Avatar
-				isInView={isInView}
-				position={new THREE.Vector3(0.5, -1, -0.6)}
-			/>
-			<Harley
-				position={new THREE.Vector3(-0.3, -0.5, -0.7)}
-				rotation={new THREE.Euler(0, Math.PI - 0.5, 0, 'XYZ')}
-				scale={new THREE.Vector3(1, 1, 1)}
-			/>
+		<>
+			<Suspense fallback={null}>
+				<Avatar
+					isInView={isInView}
+					position={new THREE.Vector3(0.5, -1, -0.6)}
+					animationName={animationName}
+				/>
+			</Suspense>
 
-			{/* <ambientLight intensity={4} /> */}
-
-			<Environment preset="warehouse"
-				ground={{
-					height: envMapHeight,
-					scale: envMapScale,
-					radius: envMapRadius
-				}} environmentIntensity={envMapIntensity} />
-		</PresentationControls>
+			<Suspense fallback={null}>
+				<Harley
+					position={new THREE.Vector3(-0.3, -0.5, -0.7)}
+					rotation={new THREE.Euler(0, Math.PI - 0.5, 0, 'XYZ')}
+					scale={new THREE.Vector3(1, 1, 1)}
+				/>
+			</Suspense>
+			
+			<Environment preset="warehouse" />
+		</>
 	)
 }
 
--- client/src/components/AvatarExperience/ThreeJsProjectsExperience.tsx
+++ client/src/components/AvatarExperience/ThreeJsProjectsExperience.tsx
@@ -1,30 +1,62 @@
-import { StrictMode, useRef } from "react"
+import { useRef, useState } from "react"
 import { Canvas } from "@react-three/fiber"
 
 import Experience from "./Experience"
 import { useInView } from "framer-motion";
 import { Leva } from "leva";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import { ActionName } from "./Avatar/Avatar";
 
 function ThreeJsProjectsExperience() {
     const canvasRef = useRef(null);
     const isInView = useInView(canvasRef, { once: true })
+    const [animationName, setAnimationName] = useState<ActionName | ''>('')
 
     return (
-        <StrictMode>
-            <Leva hidden />
-            <Canvas
-                ref={canvasRef}
-                camera={{
-                    fov: 35,
-                    near: 0.1,
-                    far: 1000,
-                    position: [1, 0.5, 4]
-                }}
-            >
-                {/* <color args={['#ff0000']} attach="background" /> */}
-                <Experience isInView={isInView} />
-            </Canvas>
-        </StrictMode>
+        <Stack height='100%'>
+            <Stack direction="row" width='100%' justifyContent='space-around' gap={5}>
+                <Button
+                    onClick={() => setAnimationName('Wave')}
+                    size='small'
+                    sx={{ backgroundColor: 'custom.blue.main', boxShadow: 7 }}
+                    variant='contained'
+                >
+                    WAVE
+                </Button>
+                <Button
+                    onClick={() => setAnimationName('Salute')}
+                    size='small'
+                    sx={{ backgroundColor: 'custom.blue.main', boxShadow: 7 }}
+                    variant='contained'
+                >
+                    Salute
+                </Button>
+                <Button
+                    onClick={() => setAnimationName('Dance')}
+                    size='small'
+                    sx={{ backgroundColor: 'custom.blue.main', boxShadow: 7 }}
+                    variant='contained'
+                >
+                    Dance
+                </Button>
+            </Stack>
+            <Box flexGrow={1}>
+                <Leva />
+                <Canvas
+                    ref={canvasRef}
+                    camera={{
+                        fov: 35,
+                        near: 0.1,
+                        far: 1000,
+                        position: [1, 0.5, 4],
+                    }}
+                >
+                    <Experience isInView={isInView} animationName={animationName} />
+                </Canvas>
+            </Box>
+        </Stack>
     )
 }
 
