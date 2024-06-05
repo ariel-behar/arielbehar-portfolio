39ab5af3b4b1cc6cd7d82e8aafe2c76b3c255760
Ariel Behar <ariel.behar@hotmail.com>
2024-06-05 07:34:02 +0300

    Repositioning the models and adding an environment map

--- client/src/components/AvatarExperience/Experience.tsx
+++ client/src/components/AvatarExperience/Experience.tsx
@@ -1,4 +1,5 @@
 import { Environment, PresentationControls } from '@react-three/drei'
+import { useControls } from 'leva'
 import * as THREE from 'three'
 
 import Avatar from './Avatar/Avatar'
@@ -11,28 +12,40 @@ interface Props {
 function Experience({
 	isInView
 }: Props) {
+	const { envMapIntensity, envMapHeight, envMapRadius, envMapScale } = useControls('environment map', {
+		envMapIntensity: { value: 1.3, min: 0, max: 12 },
+		envMapHeight: { value: 9, min: 0, max: 100 },
+		envMapRadius: { value: 48, min: 10, max: 1000 },
+		envMapScale: { value: 6, min: 5, max: 1000 }
+	})
+
 	return (
 		<PresentationControls
 			global
 			rotation={[0.13, 0.1, 0]}
 			polar={[0, 0]}
-			azimuth={[- 0.75, 0.75]}
-		// config={{ mass: 2, tension: 400 }}
+			azimuth={[- 0.35, 0.35]}
+			config={{ mass: 5, tension: 400 }}
 		// snap={{ mass: 4, tension: 400 }}
 		>
 			<Avatar
 				isInView={isInView}
-				position={new THREE.Vector3(0.4, -1, -0.6)}
+				position={new THREE.Vector3(0.5, -1, -0.6)}
 			/>
 			<Harley
-				position={new THREE.Vector3(-0.6, -0.5, -0.6)}
-				rotation={new THREE.Euler(0, Math.PI - 0.8, 0)}
-				scale={new THREE.Vector3(1)}
+				position={new THREE.Vector3(-0.3, -0.5, -0.7)}
+				rotation={new THREE.Euler(0, Math.PI - 0.5, 0, 'XYZ')}
+				scale={new THREE.Vector3(1, 1, 1)}
 			/>
 
 			{/* <ambientLight intensity={4} /> */}
 
-			<Environment preset="warehouse" environmentIntensity={1.5} />
+			<Environment preset="warehouse"
+				ground={{
+					height: envMapHeight,
+					scale: envMapScale,
+					radius: envMapRadius
+				}} environmentIntensity={envMapIntensity} />
 		</PresentationControls>
 	)
 }
