374a7c2fd22f603b559c60c2fb13f63aeb7a4eb0
Ariel Behar <ariel.behar@hotmail.com>
2024-06-07 12:54:35 +0300

    Optimizing the AboutSection for mobile

--- client/src/components/MainView/Sections/AboutSection.tsx
+++ client/src/components/MainView/Sections/AboutSection.tsx
@@ -10,16 +10,59 @@ import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
+import Stack from '@mui/material/Stack';
 
 const StyledBox = styled(Box)`
     background-color: ${({ theme }) => theme.palette.custom.black.main};
     color: ${({ theme }) => theme.palette.custom.grey.main};
 
     background-image: url("https://arielbehar-portfolio.s3.eu-central-1.amazonaws.com/ariel.png");
-    background-position: right center;
+    background-position: right bottom;
     background-repeat: no-repeat;
 
     text-shadow: 1px 1px 7px #000;
+
+    @media (max-width: 600px) {
+        background-image: none;
+    }
+
+    .float-container {
+        @media (min-width: 200px) {
+            height: 610px;
+        }
+
+        @media (min-width: 250px) {
+            height: 490px;
+        }
+
+        @media (min-width: 300px) {
+            height: 450px;
+        }
+
+        @media (min-width: 350px) {
+            height: 352px;
+        }
+
+        @media (min-width: 400px) {
+            height: 310px;
+        }
+
+        @media (min-width: 450px) {
+            height: 270px;
+        }
+
+        @media (min-width: 500px) {
+            height: 240px;
+        }
+
+        @media (min-width: 550px) {
+            height: 210px;
+        }
+
+        @media (min-width: 600px) {
+            height: auto;
+        }
+    }
 `
 interface Props {
     loadSectionHandler: (sectionNum: number) => void
@@ -29,36 +72,48 @@ function AboutSection({ loadSectionHandler }: Props) {
 
     useEffect(() => {
         loadSectionHandler(3)
-    },[loadSectionHandler])
+    }, [loadSectionHandler])
 
     return (
         <StyledBox
             component='section'
-            
-            py={{xs: 3, lg: 6 }}
+            py={{ xs: 3, lg: 6 }}
+            px={0}
             id="second-section"
-
-            sx={{
-                backgroundSize: { xs: 'cover', sm: 'inherit'}
-            }}
-
         >
-            <Container>
-                <Grid
-                    container
-                    spacing={{xs: 1, lg: 4}}
-                    p={{xs: 2, lg: 0}}
-                    sx={{
-                        backgroundColor: { xs: "rgba(0, 0, 0, 0.5)", lg: 'transparent' },
-                        borderRadius: { xs: "20px" }
-                    }}
-                >
-                    <Grid item sm={12} lg={8} >
-                        <Typography variant='h4' component='h4' mb={1}>About Me</Typography>
-                        <Typography variant='body1' component='p'>Certified Full-Stack Web Developer with over 5 years of comprehensive experience. Multilingual and self-taught in a variety of web technologies and programming languages. Proficient in working with Next.js, MERN stack, Three.js, Shopify, and WordPress. Highly reliable and self-propelled to success and professionalism with a constant hunger for knowledge and improvement.</Typography>
-                    </Grid>
+            <Container sx={{ padding: '0' }}>
+                <Stack direction='column' rowGap={1} >
+                    <Box maxWidth='900px' position='relative'>
 
-                    <Grid item sm={12} lg={8} mt={{xs: 2, lg: 0}}>
+                        <Box className="float-container" >
+                            <Typography px={2} variant='h4' component='h4' mb={1}>About Me</Typography>
+
+                            <Box
+                                display={{ xs: 'block', sm: 'none' }}
+                                height='calc(100% - 175px)'
+                                sx={{ float: 'right' }}
+                            />
+                            <Box
+                                display={{ xs: 'block', sm: 'none' }}
+                                sx={{ float: 'right', clear: 'right' }}
+                                height='175px'
+                                component="img"
+                                src="https://arielbehar-portfolio.s3.eu-central-1.amazonaws.com/ariel.png"
+                                alt="Ariel Behar Photo"
+                            />
+
+                            <Typography
+                                variant='body1'
+                                component='p'
+                                px={2}
+                                maxWidth='900px'
+                            >
+                                Certified Full-Stack Web Developer with over 5 years of comprehensive experience. Multilingual and self-taught in a variety of web technologies and programming languages. Proficient in working with Next.js, MERN stack, Three.js, Shopify, and WordPress. Highly reliable and self-propelled to success and professionalism with a constant hunger for knowledge and improvement.
+                            </Typography>
+                        </Box>
+                    </Box>
+
+                    <Box maxWidth='700px' mt={2} px={2}>
                         <Typography variant='h4' component='h4' mb={1}>Languages</Typography>
                         <Typography variant='body2' component='p' color='text.muted.light' mb={1}>
                             *Speaking / Writing / Reading /	Understanding rated from 1 to 5 (5 being the highest)
@@ -75,8 +130,8 @@ function AboutSection({ loadSectionHandler }: Props) {
                                 })
                             }
                         </Grid>
-                    </Grid>
-                </Grid>
+                    </Box>
+                </Stack>
             </Container>
         </StyledBox>
     )
