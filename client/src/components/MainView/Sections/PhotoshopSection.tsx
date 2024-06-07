a4ca23f72c592eb767de4278f99cfd9cb0f3ccdb
Ariel Behar <ariel.behar@hotmail.com>
2024-06-07 16:14:08 +0300

    Optimizing the PhotoshopSection for mobile

--- client/src/components/MainView/Sections/PhotoshopSection.tsx
+++ client/src/components/MainView/Sections/PhotoshopSection.tsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react"
 import styled from "@mui/material/styles/styled"
+import useTheme from "@mui/material/styles/useTheme"
+import useMediaQuery from "@mui/material/useMediaQuery"
+
+import Project from "../../../model/Project"
 
 import MockupProjectsCarousel from "../SectionsComponents/Photoshop/MockupProjectsCarousel"
 import Television from "../SectionsComponents/Photoshop/Television"
+import Modal from "../../Modal/Modal"
+import PsdToHtml from "../SectionsComponents/Photoshop/PsdToHtml"
 
 import Box from "@mui/material/Box"
 import Grid from "@mui/material/Grid"
 import Typography from "@mui/material/Typography"
-import Project from "../../../model/Project"
-import Modal from "../../Modal/Modal"
+import Stack from "@mui/material/Stack"
 
 const StyledBox = styled(Box)`
     border: 2px solid #2e2e2e;
@@ -26,6 +31,9 @@ interface Props {
 }
 
 function PhotoshopSection({ loadSectionHandler }: Props) {
+	const theme = useTheme();
+	const isXsSm = useMediaQuery(theme.breakpoints.down('md'));
+
 	const [modalSettings, setModalSettings] = useState<{
 		show: boolean,
 		project?: Project | null
@@ -49,18 +57,27 @@ function PhotoshopSection({ loadSectionHandler }: Props) {
 		<>
 			{(modalSettings.show && modalSettings.project) && <Modal project={modalSettings.project as Project} showModal={modalSettings.show} handleShowModal={handleShowModal} />}
 
-			<Grid container spacing={{ md: 2 }} py={7}>
+			<Grid container spacing={{ md: 2 }} py={{ xs: 3, md: 7 }}>
 				<Grid item xs={12} md={6} px={{ md: 2 }}>
-					<Box px={{ xs: 3, sm: 5, md: 7 }} pb={{ xs: 3, sm: 0, md: 0 }}>
-						<Typography variant="h4" component='h4' textAlign='center' color='text.secondary'>PSD / FIG to HTML</Typography>
+					<Typography variant="h4" component='h4' textAlign='center' color='text.secondary'>PSD / FIG to HTML</Typography>
 
-						<Typography mt={2} variant="body1" component='p' textAlign='justify' color='text.secondary'>This section focuses on my ability to work with Adobe Photoshop / Figma and accurately recreate websites into HTML format.</Typography>
-					</Box>
+					<Stack direction={{xs: 'row', md: 'column'}} justifyContent='center' alignItems='center' gap={2} mt={2}>
+						<Box>
+							<Typography mt={{md: 2}} variant="body1" component='p' textAlign='justify' color='text.secondary'>This section focuses on my ability to work with Adobe Photoshop / Figma and accurately recreate websites into HTML format.</Typography>
+						</Box>
+						<Stack height='100%' direction='column' justifyContent='center'>
+							{
+								isXsSm
+									? <PsdToHtml />
+									: <Television />
+							}
+						</Stack>
+					</Stack>
 
-					<Television />
+
 				</Grid>
 
-				<Grid item xs={12} md={6} mt={{ xs: 10, sm: 10, md: 0 }} >
+				<Grid item xs={12} md={6} mt={{ xs: 3, sm: 2, md: 0 }} >
 					<StyledBox >
 						<MockupProjectsCarousel handleShowModal={handleShowModal} />
 					</StyledBox>
