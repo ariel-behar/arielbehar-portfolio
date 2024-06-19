afc9e411986bc623fef071c3cc4783f73c811915
Ariel Behar <ariel.behar@hotmail.com>
2024-06-19 15:33:38 +0300

    Creating the LargeThumbnailPlaceholder component

---
+++ client/src/components/MainView/SectionsComponents/Projects/LargeThumbnailPlaceholder.tsx
@@ -0,0 +1,14 @@
+import Box from '@mui/material/Box'
+
+function LargeThumbnailPlaceholder() {
+    return (
+        <Box
+            component="img"
+            width='100%'
+            src='https://arielbehar-portfolio.s3.eu-central-1.amazonaws.com/projects-slides/site-plan.jpg'
+            alt='Site Plan Image'
+        />
+    )
+}
+
+export default LargeThumbnailPlaceholder
