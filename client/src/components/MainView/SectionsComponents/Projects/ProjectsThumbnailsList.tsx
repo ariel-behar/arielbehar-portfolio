bdfd469620c048b2c5611bec87263b85b09febf0
Ariel Behar <ariel.behar@hotmail.com>
2024-06-19 13:03:00 +0300

    Adding titles to the project thumbnails and adjusting their height

--- client/src/components/MainView/SectionsComponents/Projects/ProjectsThumbnailsList.tsx
+++ client/src/components/MainView/SectionsComponents/Projects/ProjectsThumbnailsList.tsx
@@ -2,6 +2,7 @@ import IProject from "../../../../model/Project"
 
 import Box from "@mui/material/Box"
 import Stack from "@mui/material/Stack"
+import Typography from "@mui/material/Typography"
 
 interface Props {
 	projects: IProject[],
@@ -13,25 +14,41 @@ function ProjectsThumbnailsList({
 	onChangeSelectedProject
 }: Props) {
 	return (
-		<Stack flexGrow="1" direction='row' alignItems='center' flexWrap='wrap' justifyContent='center' mt={2} gap={{xs: 1, md: 2}} >
+		<Stack flexGrow="1" direction='row' alignItems='center' flexWrap='wrap' justifyContent='center' mt={2} gap={{ xs: 1, md: 2 }} >
 			{
 				projects.map((project, index) => {
 					return (
-						<Box key={project._id} onClick={() => onChangeSelectedProject(project._id, index + 1)} width={{xs: '105px', md: "130px"}} height='auto'>
+						<Stack
+							key={project._id}
+							onClick={() => onChangeSelectedProject(project._id, index + 1)}
+							width={{ xs: '105px', md: '135px', lg: "130px" }}
+							height={{ xs: '105px', md: '110px', lg: "120px" }}
+							direction='column'
+							gap={1}
+							sx={{
+								cursor: 'pointer',
+								transition: 'all 0.2s',
+								'&:hover': {
+									transform: 'scale(1.1)',
+								}
+							}}
+						>
 							<Box
-								sx={{
-									cursor: 'pointer',
-									transition: 'all 0.2s',
-									'&:hover': {
-										transform: 'scale(1.1)',
-									}
-								}}
+
 								component="img"
 								width='100%'
 								src={`https://arielbehar-portfolio.s3.eu-central-1.amazonaws.com/projects-slides/${project.image}`}
 								alt={`${project.title} || ${project._id}`}
 							/>
-						</Box>
+
+							<Typography
+								variant='body2'
+								textAlign='center'
+								color='text.secondary'
+							>
+								{project.title}
+							</Typography>
+						</Stack>
 
 					)
 				})
