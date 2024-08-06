16cf53688edb375674e4e42cf9aa0c55b7a8f62c
Ariel Behar <ariel.behar@hotmail.com>
2024-08-06 18:13:59 +0300

    Restructuring the AboutSecton to include years of experience

--- client/src/components/MainView/Sections/AboutSection.tsx
+++ client/src/components/MainView/Sections/AboutSection.tsx
@@ -84,13 +84,16 @@ function AboutSection({ loadSectionHandler }: Props) {
             py={{ xs: 3, lg: 6 }}
             px={0}
             id="second-section"
+            position='relative'
         >
             <Container sx={{ padding: '0' }}>
-                <Stack direction='column' rowGap={1} >
-                    <Box maxWidth='900px' position='relative'>
+                <Stack direction='column' rowGap={1} pl={{ xs: 1, md: 0 }}>
+                    <Box position='relative'>
 
-                        <Box className="float-container" >
-                            <Typography px={2} variant='h4' component='h4' mb={2}>About Me</Typography>
+                        <Box className="float-container" maxWidth='900px'>
+                            <Stack direction='row' justifyContent='space-between' alignItems='center'>
+                                <Typography variant='h4' component='h4' mb={2}>About Me</Typography>
+                            </Stack>
 
                             <Box
                                 display={{ xs: 'block', sm: 'none' }}
@@ -110,32 +113,45 @@ function AboutSection({ loadSectionHandler }: Props) {
                                 variant='body1'
                                 component='p'
                                 px={2}
-                                maxWidth='900px'
+                                textAlign='justify'
                             >
                                 Certified Full-Stack Web Developer with over 5 years of comprehensive experience. Multilingual and self-taught in a variety of web technologies and programming languages. Proficient in working with Next.js, MERN stack, Three.js, Shopify, and WordPress. Highly reliable and self-propelled to success and professionalism with a constant hunger for knowledge and improvement.
                             </Typography>
                         </Box>
                     </Box>
 
-                    <Box maxWidth='700px' mt={2} px={2}>
-                        <Typography variant='h4' component='h4' mb={2}>Languages</Typography>
+                    <Stack direction={{ xs: 'column', sm: 'row' }} justifyItems="space-between" maxWidth='950px' mt={{md: 2}}>
 
-                        <Grid container spacing={2}>
-                            {
-                                languages.map(language => {
-                                    return (
-                                        <Grid item xs={4} sm={2} md={2} key={uuidv4()}>
-                                            <FlagCard language={language} />
-                                        </Grid>
-                                    )
-                                })
-                            }
-                        </Grid>
+                        <Box order={{ xs: 2, sm: 1 }} width={{ xs: '100%', sm: '70%' }}>
+                            <Typography variant='h4' component='h4' mb={2}>Languages</Typography>
+                            <Grid container spacing={2}>
+                                {
+                                    languages.map(language => {
+                                        return (
+                                            <Grid item xs={4} md={2} key={uuidv4()}>
+                                                <FlagCard language={language} />
+                                            </Grid>
+                                        )
+                                    })
+                                }
+                            </Grid>
 
-                        <Typography variant='body2' component='p' color='text.muted.light' my={1} textAlign="center">
-                            *Speaking / Writing / Reading /	Understanding {} rated from 1 to 5 (5 being the highest)
-                        </Typography>
-                    </Box>
+                            <Typography variant='body2' component='p' color='text.muted.light' my={1} textAlign="center">
+                                *Speaking / Writing / Reading /	Understanding { } rated from 1 to 5 (5 being the highest)
+                            </Typography>
+                        </Box>
+
+                        <Stack order={{ xs: 1, sm: 2 }} my={{ xs: 2, sm: 0 }} width={{ xs: '100%', sm: '30%' }}>
+                            <Typography variant='h4' component='h4' textAlign={{xs: 'left', md: 'center'}}>Experience</Typography>
+                            <Stack flexGrow={1} direction="column" justifyContent='center' alignItems="center" mb={{ sm: 1 }}>
+                                <Typography color='white' fontSize={130} lineHeight={0.8} component='h4' textAlign='center'>
+                                    5+
+                                </Typography>
+                                <Typography component="span" fontSize={14}>years</Typography>
+                            </Stack>
+                        </Stack>
+                    </Stack>
+
                 </Stack>
             </Container>
         </StyledBox>
